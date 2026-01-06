c540c5ebd01ee4cfc21e88fbdcafd9af40eaa0b1
Anas Elahi <m.a.elahi7475@gmail.com>
2026-01-06 15:05:31 +0500

    Updated View

--- students/views.py
+++ students/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("<h1>Hello Cloud World</h1>")
+    return HttpResponse("<h1>Hello Anas Elahi</h1>")
